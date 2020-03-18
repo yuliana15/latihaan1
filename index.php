6b3e898eaa994d9dca018c1cc3c99f10d26d8e35
yuliana15 <ulieana6@gmail.com>
2020-03-19 05:26:00 +0700

    tugas1

--- index.php
+++ index.php
@@ -2,3 +2,17 @@
   echo "hello word";
 
   ?>
+
+  <html>
+<head>
+	<title>
+		belajar php
+	</title>
+	</head>
+	<body>
+
+	<?php
+	echo "<h1>selamat datang, SITI YULIANA</h1>";
+	?>
+	</body>
+	<html>
