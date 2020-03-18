e65ee87f05353836c78719f74f5318f4d12d7548
yuliana15 <ulieana6@gmail.com>
2020-03-19 05:35:32 +0700

    tugas

--- index.php
+++ index.php
@@ -179,3 +179,178 @@ $y = 20 ;
 	echo $10 % $8;
 
 	>?
+
+
+	<?php
+  echo "hello word"
+  ?>
+
+
+  <?
+
+$nama = "saiful";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		belajar php
+
+</title>
+</head>
+<body>
+	<h1>
+		hello, selamat datang <?php echo $saiful;| ?>
+			
+		</h1>
+
+	</body>
+	</html>		
+
+
+	<?
+
+$nama = "saiful";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		belajar php
+
+</title>
+</head>
+<body>
+
+	<h1>hello, selamat datang <?php echo $saiful;| ?></h1>
+
+	</body>
+	</html>		
+
+
+	 <?php
+
+
+   //  variabel dan tipe data
+   // variabel = digunakan untuk menyimpan data
+   // Aturan Variabel 'tidak boleh diawali dengan angka,tapi boleh mengandung angka'
+
+   $nama = "Saiful Bahri";
+   ?>
+
+ <!DOCTYPE html>
+
+ <html>
+ <head>
+ 	<title>
+ 		Belajar PHP
+ 	</title>
+ </head>
+ <body>
+   <h1>Hallo, Selamat Datang <?php echo $nama; ?></h1>>
+
+   echo "Hallo, Selamat Datang $nama </br>"; #petik dua
+   echo 'Hallo, Selamat Datang $nama </br>'; #petik satu
+
+  </body>
+ </html> 
+   ?> 
+
+<?php
+Operator Aritmatika
+$x = 10 ;
+$y = 20 ;
+	echo $10 + $20;
+	echo $10 - $20;
+	echo $10 * $20;
+	echo $10 / $20;
+	echo $10 % $20;
+
+
+echo $11 + $20;
+	echo $10 - $20;
+	echo $10 * $12;
+	echo $10 / $4;
+	echo $10 % $8;
+
+	>?
+
+
+	<?php
+$nama_depan ="saiful";
+$nama_belakang ="bahri";
+echo $nama_depan ."". $nama_belakang;
+?>
+
+<?php
+
+$x = 10;
+$x +=2;
+echo $x "</br>";
+
+$y = 10;
+$y -=3;
+echo $y "</br>";
+
+$x = 10;
+$x /= 3;
+echo $x "</br>";
+
+$y = 10;
+$y %= 2;
+echo $y "</br>";
+
+?>
+
+<?php
+var_dump(1 < 5) ;
+	echo "</br>";
+
+var_dump(5 <= 4);
+	echo "</br>";
+
+var_dump(2 >= 6);
+	echo "</br>";
+
+var_dump(1 == 2);
+	echo "</br>";
+
+var_dump(1 != 4);
+
+
+var_dump (3 === "3");
+
+ var_dump (4 === "4");
+
+ var_dump (5 === "5");
+
+ var_dump (6 === "6");
+
+ var_dump (7 === "7");
+	echo "</br>";
+
+	//oprator logika
+
+	$x = 10;
+	var_dump($x < 20 && $x % 2 == 0);
+
+	$x = 13;
+ var_dump($x < 7 && $x % 3 == 0);
+
+ $x = 4;
+ var_dump($x < 12 && $x % 4 == 0);
+
+ $x = 5;
+ var_dump($x < 1 && $x % 2 == 0);
+
+ $x = 23;
+ var_dump($x < 20 && $x % 12 == 0);
+
+ $x = 11;
+ var_dump($x < 5 && $x % 2 == 0);
+
+
+
+	?>				
