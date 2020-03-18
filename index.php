7083a9cde325d85876d90f3b6505ffaad108df05
yuliana15 <ulieana6@gmail.com>
2020-03-19 05:29:51 +0700

    tugas4

--- index.php
+++ index.php
@@ -45,3 +45,32 @@ $nama = "YULIANA";
 echo "hallo,selamat datang $nama </br>";
 echo 'halle,selamat datang $nama',
 ?>
+
+
+<?php
+
+
+   //  variabel dan tipe data
+   // variabel = digunakan untuk menyimpan data
+   // Aturan Variabel 'tidak boleh diawali dengan angka,tapi boleh mengandung angka'
+
+   $nama = "SITI YULIANA";
+   ?>
+
+ <!DOCTYPE html>
+
+ <html>
+ <head>
+ 	<title>
+ 		Belajar PHP
+ 	</title>
+ </head>
+ <body>
+   <h1>Hallo, Selamat Datang <?php echo $nama; ?></h1>>
+
+   echo "Hallo, Selamat Datang $nama </br>"; #petik dua
+   echo 'Hallo, Selamat Datang $nama </br>'; #petik satu
+
+  </body>
+ </html> 
+   ?> 
