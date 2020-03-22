392005f79c4db6cfd39c3f0b3d377f54d46f22fd
yuliana15 <ulieana6@gmail.com>
2020-03-23 04:54:21 +0700

    latihan.php

---
+++ Minggu4/latihan.php
@@ -0,0 +1,18 @@
+<?php
+// pengulangan
+// for
+
+for ($i = 0; $i < 5; i++){
+	echo "Hello Word!  <br>";
+
+}
+
+/* $i = 0; bagian inisilisasi/ menetukan variabel 
+
+$1 < 5;     bagian kondisi/ubntuk memberhentikan kondisi perulanagn ketika berjumlah <
+
+i++     bagian increment  (menambah)
+kondisi
+
+
+?>
