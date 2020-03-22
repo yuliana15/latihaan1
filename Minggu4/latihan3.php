9708e5891596bc476a85ab74aba243c8e556c7a8
yuliana15 <ulieana6@gmail.com>
2020-03-23 05:27:32 +0700

    TAKS_8

---
+++ Minggu4/latihan3.php
@@ -0,0 +1,12 @@
+<?php
+$x = 10;			#kondisi variabel
+if ($x <20){    	#kondisi pengecekan dalam bracket
+
+	echo "BENAR";	#jika TRUE ini yg dicetak
+	
+} else {
+	echo "SALAH"; 	#jika FALSE ini yang di cetak
+	
+}
+
+?>
