bee951ffc8f94df781f91c7f2413f34429721fdf
unknown <ulieana6@gmail.com>
2020-04-11 13:43:10 +0700

    TASK_7

--- Minggu4/Minggu6/date.php
+++ Minggu4/Minggu6/date.php
@@ -1,5 +1,23 @@
 <?php
 //date untuk menampilkan tanggal dengan format tertentu
 
-		echo date("1", strtotime(10 june 2020))
+		function Salam ($waktu, $nama)
+		{
+			return "Selamat $waktu, $nama!";
+		}
+		?>
+
+		<!DOCTYPE html>
+		<html lang="en">
+		<head>
+
+			<meta charset="UTF-8">
+			<meta nama="viewport"
+			content="width=device-width,initial-scale=1.0">
+			<title> latihan function </title>
+		</head>
+		<body>
+			<h1><?= salam("pagi","SITI YULIANA") ?></h1>
+		</body>
+		</html>
 	?>	
