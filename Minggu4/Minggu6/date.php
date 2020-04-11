1e0dd34741d459ca17a05b1a0cfe43f0375277c3
unknown <ulieana6@gmail.com>
2020-04-11 13:29:13 +0700

    TASK_5

--- Minggu4/Minggu6/date.php
+++ Minggu4/Minggu6/date.php
@@ -1,16 +1,5 @@
 <?php
 //date untuk menampilkan tanggal dengan format tertentu
 
-		echo date(
-			"1, d-M-Y",
-			time() +
-				60 * 60 * 24 * 1000
-
-
-	$kemarin = date('Y-m-d', strtotime("-500 day", strtotime(date("Y-m-d"))));
- 			
-
-
-		);
-
+		echo date("05-07-93", mktime());
 	?>	
