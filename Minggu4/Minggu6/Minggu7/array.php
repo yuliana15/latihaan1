8ad25b3b4fe49708fdb9fb4a645f4d7d5ce25a8c
unknown <ulieana6@gmail.com>
2020-04-17 19:59:51 +0700

    Task_1

---
+++ Minggu4/Minggu6/Minggu7/array.php
@@ -0,0 +1,21 @@
+<?php
+
+$hari = array("senin","selasa","rabu","kamis","jumat");
+
+//var_dump($hari);
+//echo $hari[0];
+//print_r($hari);
+
+//echo "<br>";
+
+// $bulan = ["januari","februari","maret","april","mei","juni","juli","agustus","september","oktober","november","desember"];
+
+//print_r($bulan);
+
+
+print_r($hari);
+$hari[] = "selasa";
+echo "<br>";
+print_r($hari)
+
+?>
